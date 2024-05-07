9d3f0bb5e95cab7feaf3e0a2d0024009b19f8cac
ghnur31 <ghidayatnur@gmail.com>
2024-05-07 11:55:32 +0700

    add new starter folder and learn blade templating engine

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>about</title>
+</head>
+<body>
+  <a href="/home">home</a>
+  <a href="/about">about</a>
+  <h1>Halaman About</h1>
+  <h2>{{ $name }}</h2>
+</body>
+</html>
